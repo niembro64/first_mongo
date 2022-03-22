c9bf11f5141a93c7da4df7bee5559eb7543b733f
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-22 00:02:13 -0400

    working

--- server/controllers/sundae.controller.js
+++ server/controllers/sundae.controller.js
@@ -1,6 +1,15 @@
-const Sundae = require("../models/sundae.model"); 
+const Sundae = require("../models/sundae.model");
 // const Sundae = require("models/sundae.model");
 
+module.exports.eric = (req, res) => {
+  res.json({ message: "you're here eric" });
+};
 module.exports.testResponse = (req, res) => {
   res.json({ message: "test response eric" });
+};
+
+module.exports.findAll = (req, res) => {
+  Sundae.find({})
+    .then((results) => res.json(results))
+    .catch((err) => res.status(400).json({ message: "that didn't work", err }));
 };
--- server/routes/sundae.route.js
+++ server/routes/sundae.route.js
@@ -2,5 +2,7 @@
 const SundaeController = require("../controllers/sundae.controller");
 
 module.exports = (app) => {
+  app.get("/api", SundaeController.eric);
   app.get("/api/test", SundaeController.testResponse);
+  app.get("/api/findAll", SundaeController.findAll)
 };
